43e8d209db8a6079ad81b123598badaeb4b41e36
Renato28 <renatonobrega87@gmail.com>
2025-05-04 12:46:24 -0300

    implementando a classe CreateProductHandler

--- src/main/java/github/io/ecommerce/application/handler/CreateProductHandler.java
+++ src/main/java/github/io/ecommerce/application/handler/CreateProductHandler.java
@@ -1,4 +1,19 @@
 package github.io.ecommerce.application.handler;
 
+import github.io.ecommerce.application.command.CreateProductCommand;
+import github.io.ecommerce.domain.model.Product;
+import github.io.ecommerce.domain.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class CreateProductHandler {
+
+    private final ProductRepository repository;
+
+    public void handle(CreateProductCommand command) {
+        Product product = new Product(command);
+        repository.save(product);
+    }
 }
