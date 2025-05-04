eed63c51ff5fb1c56e33335c63a24ceca97e0a54
Renato28 <renatonobrega87@gmail.com>
2025-05-04 17:50:57 -0300

    implementando a classe User

--- src/main/java/github/io/ecommerce/domain/model/User.java
+++ src/main/java/github/io/ecommerce/domain/model/User.java
@@ -1,4 +1,16 @@
 package github.io.ecommerce.domain.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
 public class User {
+
+    private UUID id;
+    private String username;
+    private String password;
+    private String role;
 }
