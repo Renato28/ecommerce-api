81487db397caca1aae615004ada1edc12761981f
Renato28 <renatonobrega87@gmail.com>
2025-05-04 17:50:31 -0300

    implementando a classe JWTService para gerar tokens

--- src/main/java/github/io/ecommerce/infraestructure/service/JWTService.java
+++ src/main/java/github/io/ecommerce/infraestructure/service/JWTService.java
@@ -1,4 +1,35 @@
 package github.io.ecommerce.infraestructure.service;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.List;
+
+@Service
 public class JWTService {
+
+    private Key key;
+
+    public void init() {
+        this.key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    }
+
+    public String generateToken(String username, List<String> roles, String clientId) {
+        Date now = new Date();
+        Date expirationDate = new Date(now.getTime() + 3600000);
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuer("ecommerce-api")
+                .setAudience(clientId)
+                .setIssuedAt(now)
+                .setExpiration(expirationDate)
+                .claim("roles", roles)
+                .signWith(key)
+                .compact();
+    }
 }
