7aec650d1c4427628c2951679eb5d8c2c538b751
Renato28 <renatonobrega87@gmail.com>
2025-05-04 12:45:27 -0300

    implementando a classe ProductRepositoryImpl

--- src/main/java/github/io/ecommerce/infraestructure/persistence/repository/ProductRepositoryImpl.java
+++ src/main/java/github/io/ecommerce/infraestructure/persistence/repository/ProductRepositoryImpl.java
@@ -1,4 +1,50 @@
 package github.io.ecommerce.infraestructure.persistence.repository;
 
-public class ProductRepositoryImpl {
+import github.io.ecommerce.domain.model.Product;
+import github.io.ecommerce.domain.repository.ProductRepository;
+import github.io.ecommerce.infraestructure.persistence.entity.ProductEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+@RequiredArgsConstructor
+public class ProductRepositoryImpl implements ProductRepository {
+
+    private final SpringDataProductRepository repository;
+
+    @Override
+    public void save(Product product) {
+        ProductEntity entity = new ProductEntity(product);
+        repository.save(entity);
+    }
+
+    @Override
+    public Optional<Product> findById(UUID id) {
+        return repository.findById(id).map(entity ->
+                new Product(entity.getId(),
+                        entity.getName(),
+                        entity.getPrice(),
+                        entity.getQuantity()));
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return repository.findAll()
+                .stream()
+                .map(entity ->
+                new Product(entity.getId(),
+                        entity.getName(),
+                        entity.getPrice(),
+                        entity.getQuantity()))
+                .toList();
+    }
+
+    @Override
+    public void delete(UUID id) {
+        repository.deleteById(id);
+    }
 }
