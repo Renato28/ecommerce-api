bf307fe9ab630a02f2c09ee427cb274a23fd5fc5
Renato28 <renatonobrega87@gmail.com>
2025-05-04 12:42:13 -0300

    criando a entidade ProductEntity

--- src/main/java/github/io/ecommerce/infraestructure/persistence/entity/ProductEntity.java
+++ src/main/java/github/io/ecommerce/infraestructure/persistence/entity/ProductEntity.java
@@ -1,4 +1,27 @@
 package github.io.ecommerce.infraestructure.persistence.entity;
 
+import github.io.ecommerce.domain.model.Product;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "products")
+@Data
 public class ProductEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String name;
+    private BigDecimal price;
+    private int quantity;
+
+    public ProductEntity(Product product) {
+        this.name = product.getName();
+        this.price = product.getPrice();
+        this.quantity = product.getQuantity();
+    }
 }
