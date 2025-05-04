69f7b4b78ebe50476bf2cccd028f5b03b7c1851b
Renato28 <renatonobrega87@gmail.com>
2025-05-04 12:43:44 -0300

    criando a interface ProductRepository

--- src/main/java/github/io/ecommerce/domain/repository/ProductRepository.java
+++ src/main/java/github/io/ecommerce/domain/repository/ProductRepository.java
@@ -1,4 +1,13 @@
 package github.io.ecommerce.domain.repository;
 
+import github.io.ecommerce.domain.model.Product;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface ProductRepository {
+    void save(Product product);
+    Optional<Product> findById(UUID id);
+    List<Product> findAll();
 }
