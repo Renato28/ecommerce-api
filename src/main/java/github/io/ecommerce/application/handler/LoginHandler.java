6d3dc8542fe82102a9aab3d3ceab9e3390e89c51
Renato28 <renatonobrega87@gmail.com>
2025-05-04 18:26:13 -0300

    implementando a classe LoginHandler

---
+++ src/main/java/github/io/ecommerce/application/handler/LoginHandler.java
@@ -0,0 +1,32 @@
+package github.io.ecommerce.application.handler;
+
+import github.io.ecommerce.application.command.LoginCommand;
+import github.io.ecommerce.domain.exception.UserNotFoundException;
+import github.io.ecommerce.domain.model.User;
+import github.io.ecommerce.domain.repository.UserRepository;
+import github.io.ecommerce.infraestructure.service.JWTService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LoginHandler {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JWTService jwtService;
+
+    private static final String USER_NOT_FOUND = "Usuário não encontrado";
+
+    public String handle(LoginCommand command) {
+        User user = userRepository.findByUsername(command.username())
+                .orElseThrow(() -> new UserNotFoundException(USER_NOT_FOUND));
+        if (!passwordEncoder.matches(command.password(), user.getPassword())) {
+            throw new BadCredentialsException("Usuário ou senha inválida");
+        }
+
+        return jwtService.generateToken(user.getUsername(), user.getRole());
+    }
+}
