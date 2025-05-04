e9dccd0bd4640d957a17c84b61d9ee718dd23525
Renato28 <renatonobrega87@gmail.com>
2025-05-04 12:41:44 -0300

    criando a classe Product

--- src/main/java/github/io/ecommerce/domain/model/Product.java
+++ src/main/java/github/io/ecommerce/domain/model/Product.java
@@ -1,4 +1,25 @@
 package github.io.ecommerce.domain.model;
 
+import github.io.ecommerce.application.command.CreateProductCommand;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
 public class Product {
+
+    private UUID id;
+    private String name;
+    private BigDecimal price;
+    private int quantity;
+
+    public Product(CreateProductCommand command) {
+        this.name = command.name();
+        this.price = command.price();
+        this.quantity = command.quantity();
+    }
 }
