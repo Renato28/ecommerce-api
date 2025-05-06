036b95347dbcfc13786d4bf3051c11e74c6d92ea
Renato28 <renatonobrega87@gmail.com>
2025-05-05 23:32:31 -0300

    ajustando a classe LoginHandler

--- src/main/java/github/io/ecommerce/application/handler/LoginHandler.java
+++ src/main/java/github/io/ecommerce/application/handler/LoginHandler.java
@@ -4,6 +4,7 @@ import github.io.ecommerce.application.command.LoginCommand;
 import github.io.ecommerce.domain.exception.UserNotFoundException;
 import github.io.ecommerce.domain.model.User;
 import github.io.ecommerce.domain.repository.UserRepository;
+import github.io.ecommerce.domain.service.UserService;
 import github.io.ecommerce.infraestructure.service.JWTService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -17,13 +18,14 @@ public class LoginHandler {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JWTService jwtService;
+    private final UserService userService;
 
     private static final String USER_NOT_FOUND = "Usuário não encontrado";
 
     public String handle(LoginCommand command) {
         User user = userRepository.findByUsername(command.username())
                 .orElseThrow(() -> new UserNotFoundException(USER_NOT_FOUND));
-        if (!passwordEncoder.matches(command.password(), user.getPassword())) {
+        if (!userService.verifyPassword(user, command.password())) {
             throw new BadCredentialsException("Usuário ou senha inválida");
         }
 
